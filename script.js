b61c7815035d339e25abd43a835f88298ee7528d
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-25 13:34:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+.then((res) => {
+	return res.json()
+})
+.then((data) => {
+	console.log(data);
+})
+.catch((err) => {
+	console.log(err);
+})
